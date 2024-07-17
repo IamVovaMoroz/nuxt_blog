73c1b0571276ee02d9611701f6791f0336517d6d
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-07-17 18:07:52 +0200

    ..

---
+++ nuxt-blog/pages/users/[id].vue
@@ -0,0 +1,40 @@
+<!-- <script setup>
+	const route =  useRoute()
+	const id = route.params.id
+</script>
+
+
+<template>
+	<div>
+<h1>ID : {{ id }}</h1>
+	</div>
+</template> -->
+
+<script setup>
+definePageMeta({
+	middleware: [
+		function (to) {
+			if (to.params.id === '1')
+				return navigateTo('/')
+			if (to.params.id === '2')
+				return abortNavigation()
+			if (to.params.id === '3')
+				return abortNavigation('id should not be 3')
+		}
+	]
+})
+
+</script>
+
+
+<template>
+	<div>
+
+		<!-- <h1>ID : {{ $route.params.id }}</h1> -->
+	</div>
+</template>
+
+
+
+
+<style lang="scss" scoped></style>
